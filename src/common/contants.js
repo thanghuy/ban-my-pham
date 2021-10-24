448efcc1fe6e9a2cef8dd8562032fac345c246ab
Thang <huythang0903@gmail.com>
2021-10-24 21:14:22 +0700

    update file api

---
+++ src/common/contants.js
@@ -0,0 +1,11 @@
+export const API = {
+    PRODUCT: {
+        GET_PRODUCT: "/api/abc"
+    },
+    CATEGORY: {
+        GET_CATERGORY: "/api/abc"
+    },
+    CART: {
+        GET_CART: "/api/abc"
+    }
+}

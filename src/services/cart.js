166a2be3861e17adc5011127681ef8ec8e41d265
Thang <huythang0903@gmail.com>
2021-10-26 00:16:27 +0700

    update

--- src/services/cart.js
+++ src/services/cart.js
@@ -11,6 +11,39 @@ const CartService ={
             totalAmount: totalAmount,
             total: total
         }
+    },
+    handleAddToCart : (listCart, cartItem) =>{
+        const listCartItem = [];
+        if(listCart.length > 0){
+            listCart.forEach(item => {
+                if(item.idProduct === cartItem.idProduct){
+                    const amountCart = parseInt(item.amount) + parseInt(cartItem.amount);
+                    listCartItem.push({
+                        ...item,
+                        amount: amountCart,
+                        total: parseInt(amountCart) + parseInt(item.price)
+                    })
+                }else{
+                    listCartItem.push(cartItem);
+                }
+            });
+        }
+        else{
+            listCartItem.push(cartItem);
+        }
+        return listCartItem;
+    },
+    handleDeleteToCart : (listCart, idProduct) =>{
+        let totalAmount = 0;
+        let total = 0;
+        listCart.forEach(item => {
+            totalAmount += parseInt(item.amount);
+            total += (parseFloat(item.amount) * parseFloat(item.price))
+        });
+        return{
+            totalAmount: totalAmount,
+            total: total
+        }
     }
 }
 export default CartService;
